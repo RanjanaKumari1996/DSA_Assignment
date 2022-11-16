bee0c69cddfdc79678935aabe8fb4a22c886414c
RanjanaKumari1996 <112890269+RanjanaKumari1996@users.noreply.github.com>
2022-11-16 20:06:52 +0530

    Add files via upload

--- DSA_Assignment/Minimum_element_stack.py
+++ DSA_Assignment/Minimum_element_stack.py
@@ -1,47 +1,113 @@
- C
- C++
- Java
-#include <stdio.h>
-int main()
-{
-    int q;
-    scanf("%d",&q);
-    int stack[q],stackmin[q];
-    int top=-1,topmin=-1;
-    while(q--)
-    {
-        int x;scanf("%d",&x);
-        if(x==1)
-        {
-            int y;scanf("%d",&y);
-            stack[++top]=y;
-            if(topmin==-1)
-              stackmin[++topmin]=y;
-            else if(y<=stackmin[topmin])
-                stackmin[++topmin]=y;
-        }
-        else if(x==2)
-        {
-            if(top==-1)
-                printf("-1\n");
-            else
-            {
-                if(stack[top]==stackmin[topmin])
-                    topmin--;
-                //printf("%d\n",stack[top]);
-                top--;}
-        }
-        else if(x==3)
-        {
-            if(top==-1)
-                printf("-1\n");
-            else
-                printf("%d\n",stack[top]);}
-            else
-            {
-                if(top==-1)
-                    printf("-1\n");
-                else
-                    printf("%d\n",stackmin[topmin]);}
-    }
-    return 0;
+
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+ 
+
+    def __str__(self):
+        return "Node({})".format(self.value)
+ 
+    __repr__ = __str__
+ 
+ 
+class Stack:
+    def __init__(self):
+        self.top = None
+        self.count = 0
+        self.minimum = None
+ 
+
+    def __str__(self):
+        temp = self.top
+        out = []
+        while temp:
+            out.append(str(temp.value))
+            temp = temp.next
+        out = '\n'.join(out)
+        return ('Top {} \n\nStack :\n{}'.format(self.top, out))
+ 
+
+    __repr__ = __str__
+ 
+
+    def getMin(self):
+        if self.top is None:
+            return "Stack is empty"
+        else:
+            print("Minimum Element in the stack is: {}" .format(self.minimum))
+ 
+
+ 
+    def isEmpty(self):
+
+        if self.top == None:
+            return True
+        else:
+            return False
+ 
+
+    def __len__(self):
+        self.count = 0
+        tempNode = self.top
+        while tempNode:
+            tempNode = tempNode.next
+            self.count += 1
+        return self.count
+ 
+
+    def peek(self):
+        if self.top is None:
+            print("Stack is empty")
+        else:
+            if self.top.value < self.minimum:
+                print("Top Most Element is: {}" .format(self.minimum))
+            else:
+                print("Top Most Element is: {}" .format(self.top.value))
+ 
+
+    def push(self, value):
+        if self.top is None:
+            self.top = Node(value)
+            self.minimum = value
+ 
+        elif value < self.minimum:
+            temp = (2 * value) - self.minimum
+            new_node = Node(temp)
+            new_node.next = self.top
+            self.top = new_node
+            self.minimum = value
+        else:
+            new_node = Node(value)
+            new_node.next = self.top
+            self.top = new_node
+        print("Number Inserted: {}" .format(value))
+ 
+    def pop(self):
+        if self.top is None:
+            print("Stack is empty")
+        else:
+            removedNode = self.top.value
+            self.top = self.top.next
+            if removedNode < self.minimum:
+                print("Top Most Element Removed :{} " .format(self.minimum))
+                self.minimum = ((2 * self.minimum) - removedNode)
+            else:
+                print("Top Most Element Removed : {}" .format(removedNode))
+ 
+ 
+if __name__ == '__main__':
+   
+  stack = Stack()
+   
+  stack.push(15)
+  stack.push(5)
+  stack.getMin()
+  stack.push(2598)
+  stack.push(1582)
+  stack.getMin()
+  stack.pop()
+  stack.getMin()
+  stack.pop()
+  stack.peek()
